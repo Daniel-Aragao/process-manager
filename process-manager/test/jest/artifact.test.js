397baf9d340ae81a522b16b23a28a425805acd57
José Victor <jose.victor@edu.unifor.br>
2018-10-19 00:51:41 -0300

    classes teste

---
+++ process-manager/test/jest/artifact.test.js
@@ -0,0 +1,19 @@
+const artifact = require('');
+
+describe('test add artifact', ()=> {
+  it('contains artifact in ArrayList', ()=>{
+    expec(artifact.add()).toBe()
+  })
+})
+
+describe('remove add artifact', ()=> {
+  it('contains artifact in ArrayList', ()=>{
+    expec(artifact.remove()).toBe()
+  })
+})
+
+describe('edit add artifact', ()=> {
+  it('contains artifact in ArrayList', ()=>{
+    expec(artifact.edit()).toBe()
+  })
+})
