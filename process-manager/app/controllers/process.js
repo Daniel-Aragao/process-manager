97bfa4e010ef906f989c856fd2d874494883f3ee
josevictor <jose.victor@edu.unifor.br>
2018-11-11 22:19:45 -0300

    alterações controller e route

--- process-manager/app/controllers/process.js
+++ process-manager/app/controllers/process.js
@@ -7,4 +7,28 @@ module.exports.index = function(application, req, res){
             res.json(processes);
         }
     });
+
+    application.app.models.process.save((err, processes) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(processes);
+        }
+    });
+
+    application.app.models.process.remove((err, processes) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(processes);
+        }
+    });
+
+    application.app.models.process.update((err, processes) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(processes);
+        }
+    });
 }
--- process-manager/app/routes/process.js
+++ process-manager/app/routes/process.js
@@ -2,4 +2,16 @@ module.exports = function(application){
     application.get('/process/list', function(req, res){
        application.app.controllers.process.index(application, req, res);
     });
+
+    application.post('/process/add', function(req, res){
+       application.app.controllers.process.index(application, req, res);
+    });
+
+    application.post('/process/remove', function(req, res){
+       application.app.controllers.process.index(application, req, res);
+    });
+
+    application.patch('/process/update', function(req, res){
+       application.app.controllers.process.index(application, req, res);
+    });
 }
