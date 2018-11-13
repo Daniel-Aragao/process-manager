504c3968e9bf363140cde2c233824f5a19910160
josevictor <jose.victor@edu.unifor.br>
2018-11-13 01:43:38 -0300

    artefato e colaborador feitos

---
+++ process-manager/app/controllers/collaborator.js
@@ -0,0 +1,59 @@
+module.exports.index = function(application, req, res){
+    application.app.models.collaborator.find((err, collaborators) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(collaborators);
+        }
+    });
+}
+
+module.exports.find = function(application, req, res){
+    application.app.models.collaborator.findById(req.params.id, (err, collaborators) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(collaborators);
+        }
+    });
+}
+
+module.exports.save = function(application, req, res){
+    let collaborator = new application.app.models.collaborator(req.body);
+    collaborator.save((err, collaborators) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(collaborators);
+        }
+    });
+}
+
+module.exports.update = function(application, req, res){
+    application.app.models.collaborator.findById(req.body.id, (err, collaborators) => {
+        if(err){
+            console.log(err);
+        }else{
+            collaborators.name = req.body.name
+            collaborators.matricula = req.body.matricula
+
+            collaborators.save((err, collaborators) => {
+                if(err){
+                    console.log(err);
+                }else{
+                    res.json(collaborators);
+                }
+            });
+        }
+    });
+}
+
+module.exports.remove = function(application, req, res){
+    application.app.models.collaborator.findOneAndDelete({_id: req.body.id},(err, collaborators) => {
+        if(err){
+            console.log(err);
+        }else{
+            res.json(collaborators);
+        }
+    });
+}
---
+++ process-manager/app/routes/collaborator.js
@@ -0,0 +1,21 @@
+module.exports = function(application){
+    application.get('/collaborator', function(req, res){
+       application.app.controllers.collaborator.index(application, req, res);
+    });
+
+    application.get('/collaborator/:id', function(req, res){
+        application.app.controllers.collaborator.find(application, req, res);
+    });
+
+    application.post('/collaborator/add', function(req, res){
+       application.app.controllers.collaborator.save(application, req, res);
+    });
+
+    application.post('/collaborator/update', function(req, res){
+        application.app.controllers.collaborator.update(application, req, res);
+    });
+
+    application.post('/collaborator/remove', function(req, res){
+       application.app.controllers.collaborator.remove(application, req, res);
+    });
+}
