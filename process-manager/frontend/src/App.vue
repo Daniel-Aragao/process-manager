b7fac03c1168dd47ba9a3e6c604822cee9036591
Daniel Aragão <danielfilhoce@gmail.com>
2018-11-14 02:26:47 -0300

    rodando bootstrap e menu superior

--- process-manager/frontend/src/App.vue
+++ process-manager/frontend/src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <navbar-superior msg="Welcome to Your Vue.js App"/>
+    
+    <div class="container">
+      eita
+    </div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navbarSuperior from './components/navbar.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    navbarSuperior
   }
 }
 </script>
 
 <style>
-#app {
+  #app .container{
+    /* margin-top: 40px; */
+    background-color: white;
+    border-bottom-left-radius: .25rem;
+    border-bottom-right-radius: .25rem;
+    min-height: 80%;
+  }
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ process-manager/frontend/src/components/navbar.vue
@@ -0,0 +1,53 @@
+<template>
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light">
+        <!-- <a class="navbar-brand" href="#">GP</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+            <li class="nav-item btn btn-danger dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownFramework" data-toggle="dropdown">Framework</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownFramework">
+                    <a class="dropdown-item" href="#">Introdução</a>
+                </div>
+            </li>
+            <li class="nav-item btn btn-danger dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownCategorias" data-toggle="dropdown">Categorias</a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownCategorias">
+                    <a class="dropdown-item" href="#">Artefatos</a>
+                    <a class="dropdown-item" href="#">Tarefas</a>
+                    <a class="dropdown-item" href="#">Ferramentas</a>
+                    <a class="dropdown-item" href="#">Guias e Orientações</a>
+                </div>
+            </li>
+            <li class="nav-item btn btn-danger">
+                <a class="nav-link" href="#">Fluxos</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'navbar-superior'
+//   props: {
+//     msg: String
+//   }
+}
+</script>
+
+<style>
+#nav{
+    background-color: #e3f2fd;
+}
+#nav .nav-item.btn.btn-danger{
+    margin-right: 10px;
+}
+
+#nav.navbar-light .nav-link{
+    color: white;
+    font-weight: 600px;
+}
+</style>
