63e2955d2362f97b665851c3a11476b86b49b269
Daniel Aragão <danielfilhoce@gmail.com>
2018-11-14 23:21:26 -0300

    adição da logo

--- process-manager/frontend/src/App.vue
+++ process-manager/frontend/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <navbar-superior msg="Welcome to Your Vue.js App"/>
+    <navbar-superior/>
     
     <div class="container">
-      eita
+      
     </div>
   </div>
 
