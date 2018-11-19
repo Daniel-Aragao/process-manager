01f5c374758554c052120138df7a05975639df6b
josevictor <jose.victor@edu.unifor.br>
2018-11-19 08:23:53 -0300

    artifact alteracoes

--- process-manager/frontend/src/components/artifact.vue
+++ process-manager/frontend/src/components/artifact.vue
@@ -87,7 +87,7 @@ export default {
           console.log(error)
         })
       },
-      removeArtifact(index) {
+      removeArtifact() {
         artifactService.add(newArtifact).then((response) => {
         console.log(response)
         }).catch((error) => {
