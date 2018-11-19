0229993b127843181d41ccbbb68fedb6f04806f3
Daniel Aragão <danielfilhoce@gmail.com>
2018-11-19 01:53:09 -0300

    crud do artefato em andamento

--- process-manager/frontend/src/components/artifact.vue
+++ process-manager/frontend/src/components/artifact.vue
@@ -1,61 +1,106 @@
 <template>
-  <div>
-    <form>
-      <div class="row">
-        <div class="col-6">
-          <div class="form-group">
-            <label for="inputName">Nome</label>
-            <input type="email" class="form-control" id="inputName" v-model="Artifact.name" placeholder="Nome">
-          </div>
-
-        </div>
-        <div class="col-6">
-          <div class="form-group">
-            <label for="inputState">Tipo de Artefato</label>
-            <select id="inputState" class="form-control">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
-        </div>
+<div>
+  <div class="col-lg-12 edition-father">
+    <div class="edition">
+          <button class="btn btn-danger" @click="showModal">
+              Novo
+          </button>
       </div>
-
-      <div class="row">
-        <div class="col-6">
-          <div class="form-group">
-            <label for="inputEmail4">Detalhes</label>
-            <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-          </div>          
-        </div>
-        <div class="col-6">
-          <div class="form-group">
-            <label for="inputState">Processo</label>
-            <select id="inputState" class="form-control">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-6">
-          <div class="form-group">
-            <label for="inputState">Tarefa</label>
-            <select id="inputState" class="form-control">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>            
-          </div>
-        </div>
-        <div class="col-6">
-          <div class="form-group">
-            <label class="col-12">&emsp;</label>
-            <button type="submit" class="btn btn-danger" @click="addArtifact()">Salvar</button>
-          </div>
-        </div>
-      </div>
-    </form>
   </div>
+  <div class="modal" ref="artifactModal">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">{{edition? "Editar":'Criar'}} artefato</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="cancel">
+            <span aria-hidden="true">&times;</span>
+          </button> 
+        </div>
+        <div class="modal-body">
+          <form ref="artifactForm">
+            <div class="row">
+              <div class="col-6">
+                <div class="form-group">
+                  <label for="inputName">Nome</label>
+                  <input type="email" class="form-control" id="inputName" v-model="Artifact.name" placeholder="Nome">
+                </div>
+
+              </div>
+              <div class="col-6">
+                <div class="form-group">
+                  <label for="inputState">Tipo de Artefato</label>
+                  <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-6">
+                <div class="form-group">
+                  <label for="inputEmail4">Detalhes</label>
+                  <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>          
+              </div>
+              <div class="col-6">
+                <div class="form-group">
+                  <label for="inputState">Processo</label>
+                  <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-6">
+                <div class="form-group">
+                  <label for="inputState">Tarefa</label>
+                  <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                  </select>            
+                </div>
+              </div>
+              <!-- <div class="col-6">
+                <div class="form-group">
+                  <label class="col-12">&emsp;</label>
+                  <button type="submit" class="btn btn-danger" @click="addArtifact()">Salvar</button>
+                </div>
+              </div> -->
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-dark" @click="cancel">Cancelar</button>
+          <button type="button" class="btn btn-danger" @click="addArtifact">Salvar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Detalhes</th>
+          <th>Tipo</th>
+          <th>Tarefa</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="artifact in artifacts">
+          <td>{{artiface.name}}</td>
+          <td>{{artifact.description}}</td>
+          <td>{{artifact.eTypeArtifact}}</td>
+          <td>{{artifact.tasks? artifact.tasks.join(', '): ''}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
 </template>
 
 <script>
@@ -63,12 +108,31 @@ import artifactService from '@/service/artifact.service';
 
 export default {
     name: 'artefato',
+    props: {
+      processSelected: Object
+    },
     data(){
         return {
-          Artifact: {name: '', eTypeArtifact: '', details: '', process: [], tasks: [] }
+          Artifact: {name: '', eTypeArtifact: '', details: '', process: {}, tasks: [] },
+          edition: false,
+          artifacts: []
         }
-    },
+    },  
     methods: {
+      showModal(){
+        this.$refs.artifactModal.style.display = 'block'
+          
+      },
+      hideModal(){
+        this.$refs.artifactModal.style.display = 'none'
+      },
+      cancel(){
+        this.$refs.artifactForm.reset();
+        this.hideModal();
+      },
+      save(){
+        this.hideModal();
+      },
       addArtifact() {
         const newArtifact = {
           name: this.Artifact.name,
@@ -87,7 +151,7 @@ export default {
       }
     },
     mounted(){
-        this.$emit('titleChanged', 'Artefato');
+      this.$emit('titleChanged', 'Artefato');
     }
 }
 </script>
--- process-manager/frontend/src/components/processSelector.vue
+++ process-manager/frontend/src/components/processSelector.vue
@@ -1,40 +1,39 @@
 <template>
     <div id="processSelectorModal" ref="processSelectorModal" class="modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-dialog-centered" role="document"> <!--modal-lg-->
+        <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Selecione um processo</h5>
-                <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="hideModal">
-                <span aria-hidden="true">&times;</span>
-                </button> -->
-            </div>
-            <div class="modal-body">
-                <div class="col-12 form-group">
-                    <div class="input-group">
-                        <input type="text" class="form-control" placeholder="Nome do processo" v-model="newProcessName">
-                        <div class="input-group-append">
-                            <button class="btn btn-danger" type="button" id="button-addon2" @click="newProcess">+</button>
+                <div class="modal-header">
+                    <h5 class="modal-title">Selecione um processo</h5>
+                    <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click="hideModal">
+                    <span aria-hidden="true">&times;</span>
+                    </button> -->
+                </div>
+                <div class="modal-body">
+                    <div class="col-12 form-group">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Nome do processo" v-model="newProcessName">
+                            <div class="input-group-append">
+                                <button class="btn btn-danger" type="button" id="button-addon2" @click="newProcess">+</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <ul v-if="processes && processes.length" class="list-group">
-                    <li 
-                    v-for="(process, index) in processes" @click="select(index)" 
-                    class="list-group-item list-group-item-action flex-column align-items-start" 
-                        :class="{'active': isActive(index)}">                        
-                        <div class="d-flex w-100 justify-content-between">
-                            <h5 class="mb-1">{{process.name}}</h5>
-                            <span class="close" @click="remove(index)">&times;</span>
-                        </div>
-                        <small>{{process.description}}</small>
-                    </li>
-                </ul>
+                    <ul v-if="processes && processes.length" class="list-group">
+                        <li 
+                        v-for="(process, index) in processes" @click="select(index)" 
+                        class="list-group-item list-group-item-action flex-column align-items-start" 
+                            :class="{'active': isActive(index)}">                        
+                            <div class="d-flex w-100 justify-content-between">
+                                <h5 class="mb-1">{{process.name}}</h5>
+                                <span class="close" @click="remove(index)">&times;</span>
+                            </div>
+                            <small>{{process.description}}</small>
+                        </li>
+                    </ul>
 
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-danger" @click="choose">Escolher</button>
-                <!-- <button type="button" class="btn btn-secondary" @click="hideModal">Close</button> -->
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" @click="choose">Escolher</button>
+                </div>
             </div>
         </div>
     </div>
@@ -76,7 +75,7 @@ export default {
         newProcess(){
             let self = this;
 
-            let newProcess = { name:this.newProcessName, project: 20 };
+            let newProcess = { name:this.newProcessName, project: 1 };
             // this.$emit('newProcess', this.newProcessName);
 
             processService.add(newProcess, function(response){
@@ -107,7 +106,6 @@ export default {
             return this.processSelected && this.processes[index]._id == this.processSelected._id
         },
         remove(index){
-            // this.$emit('removeProcess', this.processes[index])
             let self = this;
 
             let toRemove = self.processes[index];
@@ -131,22 +129,6 @@ export default {
 </script>
 
 <style>
-#processSelectorModal .close > span{
-    color: white
-}
-#processSelectorModal .modal-header{
-    color: white;
-    background-color: #343a40
-}
-
-#processSelectorModal .modal-content{
-    border-radius: 0.4rem;
-}
-
-#processSelectorModal .modal-body{
-    max-height: 600px;
-    overflow-y: auto;
-}
 
 .list-group-item.active {
     z-index: 2;
